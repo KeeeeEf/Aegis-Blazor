f53877cdf6f29c7b23fda3dd566a58e69193a35b
H4wks123 <ivannebayer@gmail.com>
2024-12-10 23:29:54 +0800

    feat: added montserrat font

--- src/Components/Layout/MainLayout.razor.cs
+++ src/Components/Layout/MainLayout.razor.cs
@@ -2,32 +2,40 @@ using MudBlazor;
 
 namespace Aegis.Web.Components.Layout;
 
-public partial class MainLayout {
-        public MudTheme CustomTheme = new()
+public partial class MainLayout 
+{
+    public MudTheme CustomTheme = new()
+    {
+        PaletteLight = new PaletteLight()
         {
-            PaletteLight = new PaletteLight()
+            Background = "#FFFFFF",
+            PrimaryLighten = "#0033AD",
+            Primary = "#0B286D",
+            PrimaryDarken = "#000F33",
+            SecondaryLighten = "#F9F9F9",
+            Secondary = "#FFFFFF",
+            SecondaryDarken = "#495B83",
+            TextPrimary = "#323E59"
+        },
+        PaletteDark = new PaletteDark()
+        {
+            Background = "#000F33",
+            PrimaryLighten = "#000F33",
+            Primary = "#0B1A3D",
+            PrimaryDarken = "#0033AD",
+            SecondaryLighten = "#FFFFFF",
+            Secondary = "#3C92F9",
+            SecondaryDarken = "#0042E1",
+            TextPrimary = "#FFFFFF",
+        },
+        Typography = new()
+        {
+            Default = new()
             {
-                Background = "#FFFFFF",
-                PrimaryLighten = "#0033AD",
-                Primary = "#0B286D",
-                PrimaryDarken = "#000F33",
-                SecondaryLighten = "#F9F9F9",
-                Secondary = "#FFFFFF",
-                SecondaryDarken = "#495B83",
-                TextPrimary = "#323E59"
-            },
-            PaletteDark = new PaletteDark()
-            {
-                Background = "#000F33",
-                PrimaryLighten = "#000F33",
-                Primary = "#0B1A3D",
-                PrimaryDarken = "#0033AD",
-                SecondaryLighten = "#FFFFFF",
-                Secondary = "#3C92F9",
-                SecondaryDarken = "#0042E1",
-                TextPrimary = "#FFFFFF",
-            },
-        };
+                FontFamily = ["Montserrat"]
+            }
+        },
+    };
 
     public bool IsDarkMode = true;
     public void ThemeSwitcher()
